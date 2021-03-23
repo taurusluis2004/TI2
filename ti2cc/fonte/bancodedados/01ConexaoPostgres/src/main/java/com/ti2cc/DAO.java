5a59bd793d61b52f7c3eb81ec28812d2e4ba1242
Luana <1295943@sga.pucmias.br>
2021-03-23 00:00:19 -0300

    att

--- ti2cc/fonte/bancodedados/01ConexaoPostgres/src/main/java/com/ti2cc/DAO.java
+++ ti2cc/fonte/bancodedados/01ConexaoPostgres/src/main/java/com/ti2cc/DAO.java
@@ -15,8 +15,8 @@ public class DAO {
 		String mydatabase = "teste";
 		int porta = 5432;
 		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
-		String username = "ti2cc";
-		String password = "ti@cc";
+		String username = "postgres";
+		String password = "Opie123";
 		boolean status = false;
 
 		try {
